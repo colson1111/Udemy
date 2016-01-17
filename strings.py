8c04d55be9514290dd7983b822ea371c4d65e32e
Craig <colson1111@gmail.com>
2016-01-17 18:27:11 -0500

    update strings.py

--- strings.py
+++ strings.py
@@ -5,3 +5,16 @@ Created on Sun Jan 17 17:52:30 2016
 @author: Craig
 """
 
+# Single word
+'hello'
+
+# Entire phrase
+'This is also a string'
+
+# We can also use double quotes
+"String built with double quotes"
+
+# Be careful with quotes:
+# ' I'm using single quotes, but will create an error'
+
+"Now I'm ready to use the single quotes insude a string!"
