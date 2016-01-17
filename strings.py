6cad6e67ceef731ef9a9a52409c5227104afa5a8
Craig <colson1111@gmail.com>
2016-01-17 18:34:48 -0500

    update strings.py

--- strings.py
+++ strings.py
@@ -5,6 +5,7 @@ Created on Sun Jan 17 17:52:30 2016
 @author: Craig
 """
 
+# CREATING A STRING
 # Single word
 'hello'
 
@@ -18,3 +19,34 @@ Created on Sun Jan 17 17:52:30 2016
 # ' I'm using single quotes, but will create an error'
 
 "Now I'm ready to use the single quotes insude a string!"
+
+
+# PRINTING A STRING
+print 'Hello World 1'
+print 'Hello World 2'
+print 'Use \n to print a new line'
+print '\n'
+print 'See what I mean?'
+
+# Note:  Python 3 uses print('Hello World')...print is a function.
+# To get this functionality in Python 2.7, use:
+# from __future__ import print_function
+
+
+
+# STRING BASICS
+len('Hello World')
+
+# STRING INDEXING
+# Assign s as a string
+s = 'Hello World'
+
+# Check
+s
+
+# Show first element
+s[0]
+s[1]
+s[2]
+
+
