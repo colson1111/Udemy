34738d253dbeb9ce0253f5abcfeef4d10b96e7a5
Craig <colson1111@gmail.com>
2016-01-17 21:17:49 -0500

    changes made to strings.py

--- strings.py
+++ strings.py
@@ -49,4 +49,67 @@ s[0]
 s[1]
 s[2]
 
+#  Grab everything past the first term all th way to the length of s which is len(s)
+s[1:]
+
+# Note that there is no change to the original s
+s
+
+# Grab everything up to the 3rc index
+s[:3]
+
+# Everything
+s[:]
+
+# Negative indexing
+# Last letter (one index behind 0 so it loops back around)
+s[-1]
+
+# Grab everything but the last letter
+s[:-1]
+
+# Grab everything, but go in steps of size 1
+s[::1]
+
+# Grab everything, but go in step sizes of 2
+s[::2]
+
+# Use this to print a string backwards
+s[::-1]
+
+# STRING PROPERTIES
+# Strings have immutability:  once created, the elements cannot be changed
+s
+
+# Causes an error:  s[0] = 'x'
+
+# Concatenate strings
+s + 'concatenate me!'
+
+# reassign s completely
+s = s + ' concatenate me!'
+
+print s
+
+# Use multiplication to create repetition
+letter = 'z'
+letter*10
+
+# BASIC BUILT IN STRING METHODS
+
+# upper case a string
+s.upper()
+
+#lower case
+s.lower()
+
+# split by blank spaces (default for split method)
+s.split()
+
+# split by a specific element (doesn't include the elemaent that was split on)
+s.split('W')
+
+# PRINT FORMATTING
+'Insert another string with curly brackets: {}'.format('The inserted string')
+
 
