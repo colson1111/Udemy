13c32ae8bff50fc7fb0e07c460d09920b4f463e9
Craig <colson1111@gmail.com>
2016-01-24 16:56:55 -0500

    add files.py

---
+++ files.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 12:57:58 2016
+
+@author: Craig
+"""
+
+# FILES
+
+# Write a file using ipython magic function
+%%writefile test.txt
+Hello, this is a quick test file
+
+
