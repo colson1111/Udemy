480ad1bf6418cb58fa62db7797bbc492d5789bf3
Craig <colson1111@gmail.com>
2016-01-25 22:12:02 -0500

    add range file

---
+++ range.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 22:07:02 2016
+
+@author: Craig
+"""
+
+# Range function
+
+range(0, 10)
+
+x = range(0,10)
+type(x)
+
+start = 0
+stop = 20
+x = range(start, stop)
+x
+
+# specify step size as an argument
+x = range(start, stop, 2)
+x
+
+# for super large ranges, we use xrange() in python 2.
+# in python 3, we can just use range().  These functions
+# create generators that provide the list of numbers
+# for that instance, but do not store them in memory
+
+for num in range(10):
+    print num
+
+for num in xrange(10):
+    print num
+    
+# in python 2, if you do not need to save the results in a list,
+# use xrange instead of range.
